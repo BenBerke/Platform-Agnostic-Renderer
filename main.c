859668e460c6ca758f10172a07d1c5bee7c25e1f
BenBerke <berkememioglu@gmail.com>
2026-08-05 11:11:19 +0300

    Print working

--- main.c
+++ main.c
@@ -1,17 +1,94 @@
-#include "headers/renderer.h"
+#define UNICODE
+#define _UNICODE
+#include <windows.h>
 
-uint32_t r_buffer[W * H];
-int keys[256];
-struct fenster r_f = {
-    .title = "Hello World",
-    .width = W,
-    .height = H,
-    .buf = r_buffer
-};
+#define W 800
+#define H 600
 
-int main() {
-    r_init();
-    r_loop();
-    r_destroy();
-    return 0;
+static DWORD buffer[W * H];
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+    switch (uMsg) {
+        case WM_MOUSEMOVE:
+            if (wParam & MK_LBUTTON) {
+                int x = LOWORD(lParam);
+                int y = HIWORD(lParam);
+            }
+            break;
+        case WM_LBUTTONDOWN:
+            int x = LOWORD(lParam);
+            int y = HIWORD(lParam);
+            break;
+        case WM_KEYDOWN:
+            if (wParam == VK_ESCAPE) {
+                DestroyWindow(hwnd);
+            }
+            break;
+        case WM_DESTROY:
+            PostQuitMessage(0);
+            break;
+
+        case WM_PAINT:
+            PAINTSTRUCT ps;
+            HDC hdc = BeginPaint(hwnd, &ps);
+
+            BITMAPINFO bmi = {};
+            bmi.bmiHeader.biSize = sizeof(BITMAPINFOHEADER);
+            bmi.bmiHeader.biWidth = W;
+            bmi.bmiHeader.biHeight = -H;
+            bmi.bmiHeader.biPlanes = 1;
+            bmi.bmiHeader.biBitCount = 32;
+            bmi.bmiHeader.biCompression = BI_RGB;
+
+            StretchDIBits(
+                hdc,
+                0, 0, W, H,
+                0, 0, W, H,
+                buffer, &bmi,
+                DIB_RGB_COLORS, SRCCOPY
+            );
+
+            EndPaint(hwnd, &ps);
+            break;
+    }
+
+    return DefWindowProc(hwnd, uMsg, wParam, lParam);
+}
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+    const wchar_t CLASS_NAME[] = L"WindowClass";
+
+    WNDCLASSEX wc = {0};
+    wc.cbSize = sizeof(WNDCLASSEXW);
+    wc.lpfnWndProc = WindowProc;
+    wc.hInstance = hInstance;
+    wc.lpszClassName = CLASS_NAME;
+    wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+
+    if (!RegisterClassEx(&wc)) return 0;
+
+    RECT rc = {0, 0, W, H};
+
+    AdjustWindowRect(&rc, WS_OVERLAPPEDWINDOW, FALSE);
+
+    HWND hwnd = CreateWindowEx(
+      0, CLASS_NAME, L"Hello Screen",
+      WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU | WS_MINIMIZEBOX,
+      CW_USEDEFAULT, CW_USEDEFAULT,
+      rc.right - rc.left, rc.bottom - rc.top,
+      NULL, NULL, hInstance, NULL
+    );
+
+    if (hwnd == NULL) return 0;
+
+    ShowWindow(hwnd, nCmdShow);
+    UpdateWindow(hwnd);
+
+    MSG msg = {0};
+    while (GetMessage(&msg, NULL, 0, 0)) {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    return (int)msg.wParam;
 }
